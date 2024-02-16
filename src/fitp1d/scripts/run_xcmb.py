c07afde6cb1678837c7ecb97e4565804e0adb268
Naim Goksel Karacayli <ngokselk@gmail.com>
2024-02-16 10:03:14 -0500

    joint likelihood. simply code by removing mpc and vectorize options

--- src/fitp1d/scripts/run_xcmb.py
+++ src/fitp1d/scripts/run_xcmb.py
@@ -13,8 +13,6 @@ zeff = 2.4
 nwalkers = 64
 nproc = 64
 nsteps = 10000
-usePool = True
-useKms = True
 scale_cov = 5
 
 base_cosmo = {
@@ -24,8 +22,8 @@ base_cosmo = {
     'n_s': np.array([Planck18.meta['n']]),
     'ln10^{10}A_s': np.array([3.044]),
     'z': np.array([zeff]),
-    'b_F': np.array([-0.081]), 'beta_F': np.array([1.67]),
-    'k_p': np.array([11.2])  # Mpc^-1
+    'b_F': np.array([-0.15]), 'beta_F': np.array([1.67]),
+    'k_p': np.array([8.0])  # Mpc^-1
 }
 
 planck_prior = {
@@ -40,19 +38,29 @@ planck_prior = {
 
 prior_cosmo = planck_prior.copy()
 prior_cosmo.update({
-    'omega_cdm': 0.02,
-    'n_s': 0.05
+    'omega_cdm': 0.005,
+    'n_s': 0.02,
+    'ln10^{10}A_s': 0.1
 })
 
 model = LyaxCmbModel(
-    zeff, mycosmopowerdir, mywienerfilter,
-    nlnkbins=100, nwbins=10
+    zeff, mycosmopowerdir, mywienerfilter, nlnkbins=100, nwbins=10
 )
 
 boundary = model.boundary.copy()
 
-k, base_b1d = np.loadtxt(f"{myb1ddatadir}/base_b1d_24_mpc.txt", unpack=True)
-invcov = scale_cov * np.loadtxt(f"{myb1ddatadir}/base_invcov_24_mpc.txt")
+k, base_b1d = np.loadtxt(f"{myb1ddatadir}/forecast_base_b1d_24_mpc.txt", unpack=True)
+b1d_invcov = scale_cov * np.loadtxt(f"{myb1ddatadir}/forecast_base_b1d_invcov_24_mpc.txt")
+
+_, base_p1d = np.loadtxt(f"{myb1ddatadir}/forecast_base_p1d_24_mpc.txt", unpack=True)
+p1d_invcov = np.loadtxt(f"{myb1ddatadir}/forecast_base_p1d_invcov_24_mpc.txt") / scale_cov
+
+mpc2kms = model.getMpc2Kms(**base_cosmo)
+k /= mpc2kms
+base_b1d *= mpc2kms
+base_p1d *= mpc2kms
+b1d_invcov /= mpc2kms**2
+p1d_invcov /= mpc2kms**2
 
 free_params = [
     'omega_b', 'omega_cdm', 'h', 'n_s', 'ln10^{10}A_s',
@@ -60,25 +68,7 @@ free_params = [
 ]
 
 
-def _log_prob_mpc(args):
-    new_cosmo = base_cosmo.copy()
-    for i, x in enumerate(args):
-        new_cosmo[free_params[i]] = np.array([x])
-
-    for key, (b1, b2) in boundary.items():
-        v = new_cosmo[key][0]
-        if (v < b1) or (v > b2):
-            return -np.inf
-
-    prior = 0
-    for key, s in prior_cosmo.items():
-        prior += ((new_cosmo[key][0] - base_cosmo[key][0]) / s)**2
-
-    y = model.integrateB3dTrapz(k, **new_cosmo)[0] - base_b1d
-    return -0.5 * (y.dot(invcov.dot(y)) + prior)
-
-
-def _log_prob_kms(args):
+def log_prob_kms_p1d(args):
     new_cosmo = base_cosmo.copy()
     for i, x in enumerate(args):
         new_cosmo[free_params[i]] = np.array([x])
@@ -93,40 +83,31 @@ def _log_prob_kms(args):
         prior += ((new_cosmo[key][0] - base_cosmo[key][0]) / s)**2
 
     c = model.getMpc2Kms(**new_cosmo)
-    y = model.integrateB3dTrapz(k * c, **new_cosmo)[0] * c - base_b1d
-    return -0.5 * (y.dot(invcov.dot(y)) + prior)
+    y = model.getP1dTrapz(k * c, **new_cosmo)[0] * c - base_p1d
+    return -0.5 * (y.dot(p1d_invcov.dot(y)) + prior)
 
 
-def log_prob_vectorized(args):
+def log_prob_kms_joint(args):
     new_cosmo = base_cosmo.copy()
-    for i, x in enumerate(args.T):
-        new_cosmo[free_params[i]] = x
+    for i, x in enumerate(args):
+        new_cosmo[free_params[i]] = np.array([x])
 
-    new_cosmo = model.broadcastKwargs(**new_cosmo)
-
-    w = np.ones(args.shape[0], dtype=bool)
     for key, (b1, b2) in boundary.items():
-        w &= (b1 < new_cosmo[key]) & (new_cosmo[key] < b2)
+        v = new_cosmo[key][0]
+        if (v < b1) or (v > b2):
+            return -np.inf
 
-    for key in new_cosmo:
-        new_cosmo[key] = new_cosmo[key][w]
+    prior = 0
+    for key, s in prior_cosmo.items():
+        prior += ((new_cosmo[key][0] - base_cosmo[key][0]) / s)**2
 
-    result = np.empty(args.shape[0])
-    y = model.integrateB3dTrapz(k, **new_cosmo) - base_b1d
-    y = -0.5 * np.sum(y.dot(invcov) * y, axis=1)
-    result[w] = y
-    result[~w] = -np.inf
-    return result
+    c = model.getMpc2Kms(**new_cosmo)
+    y = model.getP1dTrapz(k * c, **new_cosmo)[0] * c - base_p1d
+    cost = y.dot(p1d_invcov.dot(y)) + prior
 
-
-if useKms:
-    mpc2kms = model.getMpc2Kms(**base_cosmo)
-    k /= mpc2kms
-    base_b1d *= mpc2kms
-    invcov /= mpc2kms**2
-    log_prob = _log_prob_kms
-else:
-    log_prob = _log_prob_mpc
+    y = model.integrateB3dTrapz(k * c, **new_cosmo)[0] * c - base_b1d
+    cost += y.dot(b1d_invcov.dot(y))
+    return -0.5 * cost
 
 
 def main():
@@ -134,25 +115,37 @@ def main():
     rshift = 1e-4 * np.random.default_rng().normal(size=(nwalkers, ndim))
     p0 = np.array([base_cosmo[_] for _ in free_params]).T + rshift
 
-    if usePool:
-        with mp.Pool(processes=nproc) as pool:
-            sampler = zeus.EnsembleSampler(
-                nwalkers, ndim, log_prob, vectorize=False, pool=pool
-            )
-            sampler.run_mcmc(p0, nsteps, progress=True)
-    else:
+    with mp.Pool(processes=nproc) as pool:
         sampler = zeus.EnsembleSampler(
-            nwalkers, ndim, log_prob_vectorized, vectorize=True
+            nwalkers, ndim, log_prob_kms_p1d, pool=pool
         )
         sampler.run_mcmc(p0, nsteps, progress=True)
 
-    np.savetxt(f"chains_x{scale_cov}.txt", sampler.get_chain(), header=' '.join(free_params))
-    tau = sampler.get_autocorr_time()
-    np.savetxt(f"autocorr_x{scale_cov}.txt", tau)
-    print(
-        f"Mean acceptance fraction: {np.mean(sampler.acceptance_fraction):.3f}"
-    )
-    print(f"Mean autocorrelation time: {np.mean(tau):.1f} steps")
+        np.savetxt(
+            f"chains_p1d_x{scale_cov}.txt",
+            sampler.get_chain(), header=' '.join(free_params))
+        tau = sampler.get_autocorr_time()
+        np.savetxt(f"autocorr_p1d_x{scale_cov}.txt", tau)
+        print(
+            f"Mean acceptance fraction: {np.mean(sampler.acceptance_fraction):.3f}"
+        )
+        print(f"Mean autocorrelation time: {np.mean(tau):.1f} steps")
+
+        print("Joint likelihood.")
+        sampler = zeus.EnsembleSampler(
+            nwalkers, ndim, log_prob_kms_joint, pool=pool
+        )
+        sampler.run_mcmc(p0, nsteps, progress=True)
+
+        np.savetxt(
+            f"chains_joint_x{scale_cov}.txt",
+            sampler.get_chain(), header=' '.join(free_params))
+        tau = sampler.get_autocorr_time()
+        np.savetxt(f"autocorr_joint_x{scale_cov}.txt", tau)
+        print(
+            f"Mean acceptance fraction: {np.mean(sampler.acceptance_fraction):.3f}"
+        )
+        print(f"Mean autocorrelation time: {np.mean(tau):.1f} steps")
 
 
 if __name__ == '__main__':
