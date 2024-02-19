sigma normalization and square root fix

src/fitp1d/xcmb.py
@@ -67,10 +67,11 @@ def getBispectrumTree(k, q, w, plin_interp):
 
 class MyPlinInterp(CubicSpline):
     LOG10_KMIN, LOG10_KMAX = np.log10(1e-7), np.log10(1e3)
-    S8_k = np.logspace(-4, 1, 500)
+    S8_k = np.logspace(-3.5, 1.5, 750)
     S8_log10k = np.log10(S8_k)
     S8_dlnk = np.log(S8_k[-1] / S8_k[0]) / (S8_k.size - 1)
-    W8_2 = (3 * spherical_jn(1, S8_k * 8.) / (S8_k * 8.))**2 * S8_k**3
+    S8_norm = 3 / np.pi / np.sqrt(2)
+    W8_2 = (spherical_jn(1, S8_k * 8.) / (S8_k * 8.))**2 * S8_k**3
 
     def __init__(self, log10k, log10p):
         # Add extrapolation data points as done in camb
@@ -99,7 +100,9 @@ class MyPlinInterp(CubicSpline):
 
     def sigma8(self):
         P = 10**self._interp(MyPlinInterp.S8_log10k)
-        return np.trapz(P * MyPlinInterp.W8_2, dx=MyPlinInterp.S8_dlnk, axis=-1)
+        return MyPlinInterp.S8_norm * np.sqrt(
+            np.trapz(P * MyPlinInterp.W8_2, dx=MyPlinInterp.S8_dlnk, axis=-1)
+        )
 
 
 class LyaxCmbModel(Model):